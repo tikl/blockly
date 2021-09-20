9d1eaccd7ee5269a5e3f84b4c44a1d97b738c56b
Timo Klaemt <Timo.Klaemt@tzm.de>
2021-09-20 16:27:09 +0200

    new Block festobionicActions.js

---
+++ blocks/festobionicActions.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Action blocks for Bob3.
+ * @requires Blockly.Blocks
+ * @author Evgeniya
+ */
+
+'use strict';
+
+goog.provide('Blockly.Blocks.festobionicActions');
+
+goog.require('Blockly.Blocks');
+
+Blockly.Blocks['festobionicActions_stepmotor'] = {
+    /**
+     * Turn motor on and stop motor after execution of rotations/degrees.
+     * 
+     * @constructs robActions_motor_on_for
+     * @this.Blockly.Block
+     * @param {Number}
+     *            VALUE Number of rotations/degrees
+     * @returns after execution
+     * @memberof Block
+     */
+    init: function () {
+        this.setColour(Blockly.CAT_ACTION_RGB);
+        this.appendValueInput('VALUE').appendField('Schrittmotor Umdrehungen').setCheck('Number');
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip(Blockly.Msg.MOTOR_ON_FOR_TOOLTIP_SERVO);
+    }
+};
