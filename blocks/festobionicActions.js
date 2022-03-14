7269c1ba05287683304927822dddb851895e991b
Timo Klaemt <Timo.Klaemt@tzm.de>
2022-03-14 13:44:30 +0100

    port for stepmotor

--- blocks/festobionicActions.js
+++ blocks/festobionicActions.js
@@ -22,8 +22,13 @@ Blockly.Blocks['festobionicActions_stepmotor'] = {
      * @memberof Block
      */
     init: function () {
+        var ports = getConfigPorts('stepmotor');
+        this.dependConfig = {
+            'type' : 'stepmotor',
+            'dropDown' : ports
+        };
         this.setColour(Blockly.CAT_ACTION_RGB);
-        this.appendValueInput('VALUE').appendField(Blockly.Msg.STEP_MOTOR+' '+Blockly.Msg.POSITION+' 0-120').setCheck('Number');
+        this.appendValueInput('VALUE').appendField(Blockly.Msg.STEP_MOTOR).appendField(ports).appendField(Blockly.Msg.POSITION+' 0-120').setCheck('Number');
         this.setPreviousStatement(true);
         this.setNextStatement(true);
         this.setTooltip(Blockly.Msg.MOTOR_ON_FOR_TOOLTIP_SERVO);
